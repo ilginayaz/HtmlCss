c2b1b256120b55326840629f012855b0f3af0164
ilginayaz <ilgindeniz.ayaz@bilgeadamboost.com>
2024-04-03 17:04:04 +0300

    030424

---
+++ 03.04.2024/ornek2.js
@@ -0,0 +1,111 @@
+/*
+let dizi = [];
+
+for(i=0; i<10; i++)
+{
+    let rastgele = Math.floor(Math.random()*10)+1;
+   
+        if(!dizi.includes(rastgele)){
+           dizi.push(rastgele);
+
+        }
+        else
+        i--;
+        
+    }
+    
+console.log(dizi);
+
+
+function MonotonMu(dizi1)
+{
+    let monotonMu = true;
+    for (let j = 0; j<dizi1.length; j++)
+    {
+        if(dizi1[j + 1] <= dizi1[j]){
+        monotonMu = false;
+        break;
+        }
+    }
+    if(monotonMu)
+    console.log("monoton artandır");
+    else
+    console.log("monoton artan degildir");
+}
+let a1=[25,55,655,66];
+MonotonMu(a1);
+
+*/
+
+
+
+
+/*
+
+//1
+for (var index = 0; index <= 10; index++) {
+ 
+}
+console.log(index);
+//2
+//for (let index2 = 0; index2 <= 10; index2++) 
+for (var index2 = 0; index2 <= 10; index2++){
+//let var olacak
+}
+console.log(index2);
+
+//3
+//for (const index3 = 0; index3 <= 10; index3++) 
+for (var index3 = 0; index3 <= 10; index3++){
+//const var olacak
+}
+console.log(index3);
+
+//4
+var x = 4;
+var x = 6;
+console.log(x);
+
+//5
+let y = 4;
+let y = 6;
+console.log(y);
+
+//6
+const z = 6;
+z = 77;
+console.log(z);
+
+//7
+const dizi = [1, 2, 3, 4];
+dizi[0] = 9;
+console.log(dizi[0]);
+
+//8
+const dizi2 = [1, 2, 3, 4];
+const dizi3 = [9, 10, 11, 12];
+dizi3 = dizi2;
+console.log(dizi3);
+//9
+const dizi4 = [1, 2, 4, 5];
+for (let index = 0; index < array.length; index++) {
+    console.log(dizi4[index]);
+}
+//10
+const dizi5 = [1, 2, 4, 5];
+for (const key in dizi5) {
+  dizi5[key]=44;
+  console.log(dizi5[key]);
+}
+//11
+const dizi6 = [1, 2, 4, 5];
+for (const sayi of dizi6) {
+    sayi=3;
+    console.log(sayi);
+}
+*/
+
+
+
+
+
